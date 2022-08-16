ad25a0830919c1bfeac5902ffc27b280af537624
smat67 <sheillahm@gmail.com>
2022-08-16 22:34:17 +0200

    task

---
+++ 3-quick_sort.c
@@ -0,0 +1,69 @@
+#include "sort.h"
+
+/**
+ * partition - partitions of the array
+ * @array: array to perform operation
+ * @start: start of array;
+ * @end: end of array
+ * @size: full size of array
+ * Return: position of the pivot
+ */
+int partition(int *array, int start, int end, int size)
+{
+	int pivot = array[end];
+	int i = start, j, temp;
+
+	for (j = start; j < end; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			if (i != j)
+			{
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+			i++;
+		}
+	}
+	if (i != end)
+	{
+		temp = array[i];
+		array[i] = array[end];
+		array[end] = temp;
+		print_array(array, size);
+	}
+	return (i);
+}
+
+/**
+ * quickSort - custom quick sorts using recursion approach
+ * @array: array to sort through
+ * @start: start of array or subarray
+ * @end: end of array or subarray
+ * @size: size of full array
+ */
+void quickSort(int *array, int start, int end, int size)
+{
+	int pivot;
+
+	if (start < end)
+	{
+		pivot = partition(array, start, end, size);
+		quickSort(array, start, pivot - 1, size);
+		quickSort(array, pivot + 1, end, size);
+	}
+}
+
+/**
+ * quick_sort - quick sorts an array
+ * @array: array to sort
+ * @size: size of array
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+	quickSort(array, 0, size - 1, size);
+}
